0582ee69bd6607a9095f18169b7e2784f886ac35
Jaroslav Malec <dzardacz@gmail.com>
2020-06-22 00:11:50 +0200

    Add test_motor.py

---
+++ tools/test_motor.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import random
+import serial
+import sys
+import time
+import threading
+from google.protobuf import text_format
+
+from lib import frame_send, frame_receive
+from pb import rbcx_pb2 as pb
+
+def read_serial(port):
+    while True:
+        print(text_format.MessageToString(frame_receive(port, pb.CoprocStat), as_one_line=True))
+
+
+if __name__ == "__main__":
+    with serial.Serial(sys.argv[1], baudrate=921600) as port:
+        threading.Thread(target=read_serial, args=(port, ), daemon=True).start()
+        while True:
+            power = input()
+            if (power == ''):
+                msg = pb.CoprocReq(motorReq=pb.CoprocReq.MotorReq(motorIndex=1, setBrake=0.5))
+            else:
+                msg = pb.CoprocReq(motorReq=pb.CoprocReq.MotorReq(motorIndex=1, setPower=float(power)))
+            frame_send(port, msg)
